03b7d285e52dbedd1e004898ffc687c384d22af8
PriceUA <PriceUA>
2016-03-17 22:19:07 +0200

    More minor comments added

--- my_site_testSuite_general.py
+++ my_site_testSuite_general.py
@@ -7,6 +7,9 @@ from my_site_testCase_data import mysite_testCase_data
 from my_site_testCase_deployed import mysite_testCase_deployed
 from my_site_testCase_JQeury_scripts import mysite_testCase_JQuery_scripts
 
+# Test suite with all the test cases merged. All tests could be launched from here (added or removed).
+# Couple of test runners are also available from here.
+
 dir = os.getcwd()
 
 mysite_testCase_deployed = unittest.TestLoader().loadTestsFromTestCase(mysite_testCase_deployed)
@@ -24,6 +27,7 @@ runner = HTMLTestRunner.HTMLTestRunner(
 	description="Smoke Tests"
 )
 
-#xmlrunner.XMLTestRunner(verbosity=2, output='test-reports').run(smoke_tests)
-runner.run(smoke_tests)
-#unittest.TextTestRunner(verbosity=2).run(smoke_tests)
+
+#xmlrunner.XMLTestRunner(verbosity=2, output='test-reports').run(smoke_tests) # Uncomment to use generate JUnit format report to use with Jenkins
+runner.run(smoke_tests) # Uncomment to use HTML runner to get HTML report
+#unittest.TextTestRunner(verbosity=2).run(smoke_tests) # Simple runner
